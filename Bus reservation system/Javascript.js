ceee2de9e25debd3e353f22dfaad74f727bf81f7
Diego Martínez <56555056+Spicygarn@users.noreply.github.com>
2021-05-01 15:28:38 -0500

    Update Javascript.js

--- Bus reservation system/Javascript.js
+++ Bus reservation system/Javascript.js
@@ -5,13 +5,13 @@ window.onload = function load() {
 
 const createStorage = () => {
   for (var i = 0; i < 35; i++) {
-    localStorage.setItem("seat_" + i, "Avaible");
+    localStorage.setItem('seat_' + i, 'Avaible');
   }
 };
 
 const verifyStorage = () => {
   for (var i = 0; i < 35; i++) {
-    if (localStorage.getItem("seat_" + i) === null) {
+    if (localStorage.getItem('seat_' + i) === null) {
       createStorage();
     }
   }
@@ -19,57 +19,57 @@ const verifyStorage = () => {
 
 function update() {
   for (var i = 0; i < 35; i++) {
-    id = "n" + i;
-    item = localStorage.getItem("seat_" + i);
+    id = 'n' + i;
+    item = localStorage.getItem('seat_' + i);
     document.getElementById(id).innerHTML = item;
   }
 }
 
 function clicktb(clicked_id) {
-  document.getElementById("num_seat").value = parseInt(clicked_id) + 1;
+  document.getElementById('num_seat').value = parseInt(clicked_id) + 1;
 }
 
 function resetinput() {
-  document.getElementById("num_seat").value = 1;
-  document.getElementById("name_seat").value = "";
+  document.getElementById('num_seat').value = 1;
+  document.getElementById('name_seat').value = '';
 }
 
 function save() {
-  var numSeat = parseInt(document.getElementById("num_seat").value);
-  var nameSeat = document.getElementById("name_seat").value;
-  if (localStorage.getItem("seat_" + (numSeat - 1)) != "Avaible") {
+  var numSeat = parseInt(document.getElementById('num_seat').value);
+  var nameSeat = document.getElementById('name_seat').value;
+  if (localStorage.getItem('seat_' + (numSeat - 1)) != 'Avaible') {
     alert("The seat isn't avaible");
-  } else if (nameSeat === "") {
-    alert("the field cannot be empty");
+  } else if (nameSeat === '') {
+    alert('the field cannot be empty');
   } else {
-    localStorage.setItem("seat_" + (numSeat - 1), nameSeat);
+    localStorage.setItem('seat_' + (numSeat - 1), nameSeat);
     resetinput();
     update();
   }
 }
 
 function edit() {
-  var numSeat = parseInt(document.getElementById("num_seat_modal").value);
-  var nameSeat = document.getElementById("name_seat_modal").value;
-  if (localStorage.getItem("seat_" + (numSeat - 1)) === "Avaible") {
-    alert("Seat must be reserved");
-  } else if (nameSeat === "") {
-    alert("the field cannot be empty");
+  var numSeat = parseInt(document.getElementById('num_seat_modal').value);
+  var nameSeat = document.getElementById('name_seat_modal').value;
+  if (localStorage.getItem('seat_' + (numSeat - 1)) === 'Avaible') {
+    alert('Seat must be reserved');
+  } else if (nameSeat === '') {
+    alert('the field cannot be empty');
   } else {
-    localStorage.setItem("seat_" + (numSeat - 1), nameSeat);
+    localStorage.setItem('seat_' + (numSeat - 1), nameSeat);
     resetinput();
     update();
   }
 }
 
 function del() {
-  var numberSeat = parseInt(document.getElementById("num_seat").value);
-  if (localStorage.getItem("seat_" + (numberSeat - 1)) === "Avaible") {
-    alert("Seat must be reserved");
+  var numberSeat = parseInt(document.getElementById('num_seat').value);
+  if (localStorage.getItem('seat_' + (numberSeat - 1)) === 'Avaible') {
+    alert('Seat must be reserved');
   } else {
-    var answer = window.confirm("Are you sure?");
+    var answer = window.confirm('Are you sure?');
     if (answer) {
-      localStorage.setItem("seat_" + (numberSeat - 1), "Avaible");
+      localStorage.setItem('seat_ '+ (numberSeat - 1), 'Avaible');
       resetinput();
       update();
     }
